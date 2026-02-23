82033fb206bd1037093c53754930b07e50793ab9
venelinkolev <114242353+venelinkolev@users.noreply.github.com>
2026-02-23 14:04:56 +0200

    Set Firebase Realtime Database

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { FIREBASE_CONFIG } from "./local.environment";
+import { getDatabase } from "firebase/database";
+import { ENVIRONMENT } from "./local.environment";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = FIREBASE_CONFIG;
+const firebaseConfig = ENVIRONMENT.firebase;
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+// Initialize Realtime Database and get a reference to the service
+export const database = getDatabase(app);
